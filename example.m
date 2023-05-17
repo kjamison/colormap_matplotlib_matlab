6cf266535c75571aad708775f126b00bc16a3f22
kjamison <keith.jamison@gmail.com>
2023-05-17 15:00:27 -0400

    Updated version to add complementary colors

--- example.m
+++ example.m
@@ -1,5 +1,26 @@
+%% display colormaps
 colormapnames=colormap_matplotlib('list');
 colormapnames=colormapnames(cellfun(@(x)~endsWith(x,'_r'),colormapnames));
+colormapnames=colormapnames(cellfun(@(x)~endsWith(x,'_comp'),colormapnames));
+
+figure('color',[1 1 1],'defaulttextinterpreter','none');
+set(gcf,'position',get(0,'screensize'));
+
+for c = 1:numel(colormapnames)
+    subplot(ceil(sqrt(numel(colormapnames))),ceil(sqrt(numel(colormapnames))),c);
+    imagesc(linspace(0,1,256));
+    colormap(gca,colormap_matplotlib(colormapnames{c}));
+    title(colormapnames{c});
+    set(gca,'xtick',[],'ytick',[]);
+    box on;
+    fprintf('%s\n',colormapnames{c});
+end
+
+%% display complementary colormaps (if present)
+colormapnames=colormap_matplotlib('list');
+colormapnames=colormapnames(cellfun(@(x)~endsWith(x,'_r'),colormapnames));
+colormapnames=colormapnames(cellfun(@(x)endsWith(x,'_comp'),colormapnames));
+
 figure('color',[1 1 1],'defaulttextinterpreter','none');
 set(gcf,'position',get(0,'screensize'));
 
--- save_matplotlib_colormaps.py
+++ save_matplotlib_colormaps.py
@@ -1,14 +1,48 @@
 import matplotlib
 import matplotlib.pyplot as plt
+from matplotlib.colors import LinearSegmentedColormap
 import numpy as np
 import sys
 from scipy.io import savemat
 
-def write_cmap_matfile(outfile,n=256):
+def complementary_colormap(cmap,new_cmap_name='complementary_cmap'):
+    from colorspacious import cspace_converter
+
+    v=np.linspace(0,1,256)
+    try:
+        base_colors=cmap(v)[:,:3]
+    except TypeError:
+        base_colors=cmap.copy()[:,:3]
+    
+    lab_colors=cspace_converter("sRGB1", "JCh")(base_colors[np.newaxis,:,:])
+    new_colors=lab_colors
+    new_colors[0,:,2]=(lab_colors[0,:,2]+180) % 360
+    new_rgb=cspace_converter("JCh","sRGB1")(new_colors)
+    new_rgb=np.clip(new_rgb,0,1)
+    new_cmap=LinearSegmentedColormap.from_list(new_cmap_name, new_rgb[0,:,:])
+
+    return new_cmap
+
+def write_cmap_matfile(outfile,n=256,add_complementary=False):
+
     C={}
     for cname in plt.colormaps():
+        print("Adding %s" % (cname))
         cmap = plt.get_cmap(cname, n)(range(n))
         C[cname]=cmap[:,:3]
+
+    if add_complementary:
+        for cname in plt.colormaps():
+            if cname.endswith("_r"):
+                new_cname=cname[:-2]+"_comp_r"
+            else:
+                new_cname=cname+"_comp"
+            
+            new_cmap = complementary_colormap(plt.get_cmap(cname, n))
+
+            print("Adding %s" % (new_cname))
+            cmap = new_cmap(range(n))
+            C[new_cname]=cmap[:,:3]
 
     C["version_matplotlib"]=matplotlib.__version__
     C["version_python"]="%d.%d.%d" % (sys.version_info.major,sys.version_info.minor,sys.version_info.micro)
@@ -19,4 +53,4 @@ if __name__ == '__main__':
     n=256
     if len(sys.argv) > 2:
         n=int(sys.argv[2])
-    write_cmap_matfile(outfile,n)
+    write_cmap_matfile(outfile,n,add_complementary=True)
